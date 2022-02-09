52d56fe8839d65198b762d9a98c28dd709f786d5
thanhntk8 <thanhntk8@fpt.com.vn>
2022-02-09 16:05:32 +0700

    Trie

--- BIGO/lecture_14/Trie/Consistency_Checker.py
+++ BIGO/lecture_14/Trie/Consistency_Checker.py
@@ -1,11 +1,166 @@
 # python 3
+class Node:
+   def __init__(self):
+      self.end_word = False
+      self.child = dict()
 
+def addWord(root, s):
+   temp = root
+   s_prefix_other = False
+   other_prefix_s = True
+   
+   for ch in s:
+      if ch not in temp.child:
+         temp.child[ch] = Node()
+         other_prefix_s = False
+      
+      temp = temp.child[ch]
+      if temp.end_word:
+         s_prefix_other = True
+   
+   temp.end_word = True
+   return s_prefix_other or other_prefix_s
+
+t = int(input())
+for tc in range(t):
+   n = int(input())
+   root = Node()
+   is_inconsistent = False
+
+   for _ in range(n):
+      if not is_inconsistent:
+         is_inconsistent = addWord(root, input())
+      else:
+         input()
+
+   print('Case {}: {}'.format(tc + 1, 'NO' if is_inconsistent else 'YES'))
 # C++
 """
+#include <iostream>
+#include <vector>
+using namespace std;
+const int MAX = 10;
 
+struct Node {
+   Node* child[MAX];
+   bool end_word;
+};
+
+Node *newNode() {
+   Node *new_node = new Node;
+   new_node->end_word = false;
+   for (int i = 0; i < MAX; i++) {
+      new_node->child[i] = nullptr;
+   }
+   return new_node;
+}
+
+bool addWord(Node* root, const string& s) {
+   Node* temp = root;
+   bool s_prefix_other = false;
+   bool other_prefix_s = true;
+
+   for (int i = 0; i < s.size(); i++) {
+      int ch = s[i] - '0';
+
+      if (temp->child[ch] == nullptr) {
+         temp->child[ch] = newNode();
+         other_prefix_s = false;
+      }
+
+      temp = temp->child[ch];
+      if (temp->end_word) {
+         s_prefix_other = true;
+      }
+   }
+
+   temp->end_word = true;
+   return (s_prefix_other || other_prefix_s);
+}
+      
+int main() {
+   int t, n, tc;
+   string s;
+   cin >> t;
+
+   for (tc = 1; tc <= t; tc++) {
+      cin >> n;
+      Node* root = newNode();
+      bool is_inconsistent = false;
+
+      for (int i = 0; i < n; i++) {
+         cin >> s;
+         if (!is_inconsistent) {
+            is_inconsistent = addWord(root, s);
+         }
+      }
+
+      cout << "Case " << tc << ": " << (is_inconsistent ? "NO" : "YES") << endl;
+   }
+   return 0;
+}
 """
 # JAVA
 """
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
+class Node {
+    static final int MAX = 10;
+    public Node[] child;
+    public boolean isLeaf;
+    public Node() {
+        child = new Node[MAX];
+        isLeaf = false;
+    }
+}
+class Trie {
+    Node root;
+    public Trie() {
+        root = new Node();
+    }
+    public boolean add(String s) {
+        Node p = root;
+        boolean flag = false;
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i) - '0';
+            if (p.child[c] == null) {
+                p.child[c] = new Node();
+                flag = true;
+            }
+            p = p.child[c];
+            if (p.isLeaf) {
+                flag = false;
+                break;
+            }
+        }
+        p.isLeaf = true;
+        return flag;
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(sc.nextLine());
+        for (int t = 1; t <= T; t++) {
+            System.out.print("Case " + t + ": ");
+            Trie trie = new Trie();
+            int n = Integer.parseInt(sc.nextLine());
+            boolean flag = true;
+            while (n-- > 0) {
+                String s = sc.nextLine();
+                if (flag && !trie.add(s)) 
+                    flag = false;
+            }
+            if (flag)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+        System.exit(0);
+    }
+}
 """
 
--- BIGO/lecture_14/Trie/Search_Engine.py
+++ BIGO/lecture_14/Trie/Search_Engine.py
@@ -1,11 +1,168 @@
 # python 3
+class Node:
+    def __init__(self):
+        self.child = dict()
+        self.maxValue = -1
 
+def addWord(root, s, value):
+    tmp = root
+    for c in s:
+        if c not in tmp.child:
+            tmp.child[c] = Node()
+        tmp = tmp.child[c]
+        tmp.maxValue = max(tmp.maxValue, value)
+
+def getHeighestMatching(root, s):
+    tmp = root
+    for c in s:
+        if c not in tmp.child:
+            return -1
+        tmp = tmp.child[c]
+    return tmp.maxValue
+
+n, q = map(int, input().strip().split())
+root = Node()
+
+for _ in range(n):
+    line = input().strip().split()
+    addWord(root, line[0], int(line[1]))
+
+for _ in range(q):
+    s = input().strip()
+    print(getHeighestMatching(root, s))
 # C++
 """
+#include <bits/stdc++.h>
+#define ALPHABET_SIZE 26
+using namespace std;
 
+struct Node {
+    struct Node *child[ALPHABET_SIZE];
+    int maxValue;
+};
+
+struct Node *newNode() {
+    struct Node *pNode = new struct Node;
+    pNode->maxValue = -1;
+
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+        pNode->child[i] = nullptr;
+    }
+    
+    return pNode;
+}
+
+void addWord(struct Node *root, string s, int value) {
+    struct Node *tmp = root;
+    
+    for (char &c : s) {
+        int pos = c - 'a';
+        if (!tmp->child[pos]) {
+            tmp->child[pos] = newNode();
+        }
+        
+        tmp = tmp->child[pos];
+        tmp->maxValue = max(tmp->maxValue, value);
+    }
+}
+
+int getHighestMatching(struct Node *root, string s) {
+    struct Node *tmp = root;
+
+    for (char &c : s) {
+        int pos = c - 'a';
+        if (!tmp->child[pos]) {
+            return -1;
+        }
+        tmp = tmp->child[pos];
+    }
+    
+    return tmp->maxValue;
+}
+
+int main() {
+    int n, q, value;
+    string s;
+    cin >> n >> q;
+    struct Node *root = newNode();
+
+    for (int i = 0; i < n; i++) {
+        cin >> s >> value;
+        addWord(root, s, value);
+    }
+
+    for (int i = 0; i < q; i++) {
+        cin >> s;
+        cout << getHighestMatching(root, s) << endl;
+    }
+
+    return 0;
+}
 """
 # JAVA
 """
+import java.util.*;
 
+public class Main {
+    static final int ALPHABET_SIZE = 26;
+    
+    static class Node {
+        Node[] child = new Node[ALPHABET_SIZE];
+        int maxValue;
+    
+        Node() {
+            maxValue = -1;
+            for (int i = 0; i < ALPHABET_SIZE; i++) {
+                child[i] = null;
+            }
+        }
+    }
+    
+    static void addWord(Node root, String s, int value) {
+        Node tmp = root;
+    
+        for (char c : s.toCharArray()) {
+            int pos = c - 'a';
+            if (tmp.child[pos] == null) {
+                tmp.child[pos] = new Node();
+            }
+    
+            tmp = tmp.child[pos];
+            tmp.maxValue = Math.max(tmp.maxValue, value);
+        }
+    }
+    
+    static int getHighestMatching(Node root, String s) {
+        Node tmp = root;
+    
+        for (char c : s.toCharArray()) {
+            int pos = c - 'a';
+            if (tmp.child[pos] == null) {
+                return -1;
+            }
+            tmp = tmp.child[pos];
+        }
+    
+        return tmp.maxValue;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        Node root = new Node();
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            int value = sc.nextInt();
+            addWord(root, s, value);
+        }
+
+        for (int i = 0; i < q; i++) {
+            String s = sc.next();
+            System.out.println(getHighestMatching(root, s));
+        }
+    }
+}
 """
 
